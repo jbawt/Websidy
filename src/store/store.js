e7bc8d8298afa114be7c1310468a78d5170440ec
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2026-05-06 23:45:28 -0600

    comment site theme slicer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import themeReducer from './slices/themeSlice'
 
+// slice for site theme management
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
   },
 })
-
